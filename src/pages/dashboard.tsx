7f929aaebc5d1a647599bd3d74fe983390c9f652
Kunal-ryfl <kunalsharma070806@gmail.com>
2023-03-18 12:07:33 +0530

    g

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import Todos from '~/components/Todos'
-import {signOut, useSession } from "next-auth/react";
+
+import {signOut} from "next-auth/react";
 
 import {AiOutlinePoweroff} from 'react-icons/ai'
 import Link from 'next/link';
 
 const dashboard = () => {
-    const {data:sessionData} = useSession()
       return (
     <div className=' text-white min-h-screen flex  p-2 flex-col items-center  bg-black'> 
        <div className=' text-black flex items-center justify-between px-4 py-4 w-full my-4  md:w-[750px] bg-yellow-600  rounded-xl'>
-       <h1 className=' text-3xl'>{sessionData?.user.name}</h1>
+       <h1 className=' text-3xl'>Dashboard</h1>
 <div className=' flex justify-center items-center'>
 
        <button onClick={()=>signOut()}>
